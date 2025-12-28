d85bb48e52fd33e358360c91c49089779fedc9f9
coluccifrancesco <coluccif2002@gmail.com>
2025-12-28 15:58:49 +0100

    feat: update navigation links in edit views; change route references to show specific difficulty and technology

--- documentation-laravel/resources/views/technologies/edit.blade.php
+++ documentation-laravel/resources/views/technologies/edit.blade.php
@@ -49,7 +49,7 @@
 
                 <div class="d-flex justify-content-between align-items-center mt-4">
                     <button class="btn btn-primary" type="button">
-                        <a href="{{ route('technologies.index') }}" class="text-white link-underline link-underline-opacity-0">Go back<i class="ms-2 fa-solid fa-arrow-left"></i></a>
+                        <a href="{{ route('technologies.show', $technology->id) }}" class="text-white link-underline link-underline-opacity-0">Go back<i class="ms-2 fa-solid fa-arrow-left"></i></a>
                     </button>
                     
                     <input type="submit" value="Save" class="btn btn-success">
