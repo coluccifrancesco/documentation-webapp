78221adeb48c7c7131d39bf0d6f5389d4fcf83e7
coluccifrancesco <coluccif2002@gmail.com>
2025-12-29 17:32:35 +0100

    refactor: remove unused useState import and clean up App component

--- documentation-react/src/App.jsx
+++ documentation-react/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    
+    // Se hai problemi con le CORS aggiusta .env di Laravel 
 
     return (
         <>
