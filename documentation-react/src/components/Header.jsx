8f9e3f00f0d7c678b1f723067536e98859d90a54
coluccifrancesco <coluccif2002@gmail.com>
2026-01-02 13:05:58 +0100

    feat: update Header component layout and styles; add global CSS reset

--- documentation-react/src/components/Header.jsx
+++ documentation-react/src/components/Header.jsx
@@ -1,8 +1,26 @@
 export default function Header () {
 
-    return <>
+    return <header className="w-100 py-2 px-4 d-flex align-items-center justify-content-between bg-dark text-white">
     
-    <h1>Header</h1>
+        <div className="d-flex align-items-center gap-4">
+            <h1>DocuHub</h1>
+
+            <ul className="list-unstyled d-flex align-items-center gap-4">
+                <li>
+                    <a href="/topics" className="text-white">Topics</a>
+                </li>
+
+                <li>
+                    <a href="/technologies" className="text-white">Technologies</a>
+                </li>
+
+                <li>
+                    <a href="/difficulties" className="text-white">Difficulties</a>
+                </li>
+            </ul>
+        </div>
+
+        <div></div>
     
-    </>
+    </header>
 }
