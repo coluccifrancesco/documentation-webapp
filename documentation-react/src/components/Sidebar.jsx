5bf0d05946f8efa74d9e467f480275af633ea0dd
coluccifrancesco <coluccif2002@gmail.com>
2026-01-05 16:26:06 +0100

    style: enhance Sidebar component with new styles and improve Header layout

--- documentation-react/src/components/Sidebar.jsx
+++ documentation-react/src/components/Sidebar.jsx
@@ -2,22 +2,24 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header () {
 
-    return <div className="d-none d-lg-block col-lg-3 py-4 px-5 text-white bg-dark">
+    return <div className="d-none d-lg-block col-lg-3 py-2 px-4">
     
-        <div className="">
-            <Link to="/"><h1 className='text-white'>DocuHub</h1></Link>
-
-            <ul className="list-unstyled">
-                <li className='my-3'>
-                    <NavLink to="/topics"><h5 className='text-white'>Topics</h5></NavLink>
+        <div className="sidebar">
+            <ul className="list-unstyled p-4">
+                <li className=''>
+                    <Link to="/"><h1 className='text-color-500'>DocuHub</h1></Link>
                 </li>
 
-                <li className='my-3'>
-                    <NavLink to="/technologies"><h5 className='text-white'>Technologies</h5></NavLink>
+                <li className='sidebar-link px-3 py-2 my-2'>
+                    <NavLink to="/topics"><h5 className='text-color-500'>Topics</h5></NavLink>
                 </li>
 
-                <li className='my-3'>
-                    <NavLink to="/difficulties"><h5 className='text-white'>Difficulties</h5></NavLink>
+                <li className='sidebar-link px-3 py-2 my-2'>
+                    <NavLink to="/technologies"><h5 className='text-color-500'>Technologies</h5></NavLink>
+                </li>
+
+                <li className='sidebar-link px-3 py-2 my-2'>
+                    <NavLink to="/difficulties"><h5 className='text-color-500'>Difficulties</h5></NavLink>
                 </li>
             </ul>
         </div>
