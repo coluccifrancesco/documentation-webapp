14535f68a1c9ccf26bae4222f4fc757487c876c1
coluccifrancesco <coluccif2002@gmail.com>
2026-01-05 15:52:06 +0100

    fix: update Sidebar component for consistent styling and improved structure

--- documentation-react/src/components/Sidebar.jsx
+++ documentation-react/src/components/Sidebar.jsx
@@ -2,22 +2,22 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header () {
 
-    return <div className="d-none d-lg-block col-lg-3 p-4 bg-dark text-white">
+    return <div className="d-none d-lg-block col-lg-3 py-4 px-5 text-white bg-dark">
     
         <div className="">
             <Link to="/"><h1 className='text-white'>DocuHub</h1></Link>
 
             <ul className="list-unstyled">
-                <li>
-                    <NavLink to="/topics"><p className='text-white'>Topics</p></NavLink>
+                <li className='my-3'>
+                    <NavLink to="/topics"><h5 className='text-white'>Topics</h5></NavLink>
                 </li>
 
-                <li>
-                    <NavLink to="/technologies"><p className='text-white'>Technologies</p></NavLink>
+                <li className='my-3'>
+                    <NavLink to="/technologies"><h5 className='text-white'>Technologies</h5></NavLink>
                 </li>
 
-                <li>
-                    <NavLink to="/difficulties"><p className='text-white'>Difficulties</p></NavLink>
+                <li className='my-3'>
+                    <NavLink to="/difficulties"><h5 className='text-white'>Difficulties</h5></NavLink>
                 </li>
             </ul>
         </div>
