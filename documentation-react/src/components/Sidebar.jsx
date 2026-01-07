4c32d57b4cc8ca4535a3b294ba2fb42a4ae1cb51
coluccifrancesco <coluccif2002@gmail.com>
2026-01-07 17:08:33 +0100

    adjusted sidebar and header's style

--- documentation-react/src/components/Sidebar.jsx
+++ documentation-react/src/components/Sidebar.jsx
@@ -6,14 +6,14 @@ export default function Header () {
            
         <ul className="list-unstyled p-4">
             <li className='sidebar-link px-2 py-1'>
-                <Link to="/"><h1 className='text-color-500'>DocuHub</h1></Link>
+                <Link to="/"><h1>DocuHub</h1></Link>
             </li>
 
             <li className='sidebar-link px-3 py-2 my-2'>
                 <NavLink to="/topics">
                     <div className='d-flex align-items-center justify-content-start gap-3'>
-                        <h5 className='text-color-500'>Topics</h5>
-                        <i className="text-color-500 fa-solid fa-book-open"></i>
+                        <h5>Topics</h5>
+                        <i className="fa-solid fa-book-open"></i>
                     </div>
                 </NavLink>
             </li>
@@ -21,8 +21,8 @@ export default function Header () {
             <li className='sidebar-link px-3 py-2 my-2'>
                 <NavLink to="/technologies">
                     <div className='d-flex align-items-center justify-content-start gap-3'>
-                        <h5 className='text-color-500'>Technologies</h5>
-                        <i className="text-color-500 fa-solid fa-microchip"></i>
+                        <h5>Technologies</h5>
+                        <i className="fa-solid fa-microchip"></i>
                     </div>
                 </NavLink>
             </li>
@@ -30,8 +30,8 @@ export default function Header () {
             <li className='sidebar-link px-3 py-2 my-2'>
                 <NavLink to="/difficulties">
                     <div className='d-flex align-items-center justify-content-start gap-3'>
-                        <h5 className='text-color-500'>Difficulties</h5>
-                        <i className="text-color-500 fa-solid fa-dumbbell"></i>
+                        <h5>Difficulties</h5>
+                        <i className="fa-solid fa-dumbbell"></i>
                     </div>
                 </NavLink>
             </li>
