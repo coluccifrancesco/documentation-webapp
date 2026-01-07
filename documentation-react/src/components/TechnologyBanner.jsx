bb431039b0ba4e86859ad240d613736c656aa151
coluccifrancesco <coluccif2002@gmail.com>
2026-01-07 20:04:20 +0100

    refactored tech banner code

--- documentation-react/src/components/TechnologyBanner.jsx
+++ documentation-react/src/components/TechnologyBanner.jsx
@@ -4,7 +4,13 @@ export default function TechnologyBanner (props) {
     // <TechnologyBanner name={tech.name} bg_color={tech.bg_color} font_color={tech.font_color} />
 
     return (
-        <div className='difficulty-banner py-2 px-3 rounded text-center' style={{ backgroundColor:`${props.bg_color}`, color:`${props.font_color}` }}>
+        <div 
+            className='difficulty-banner py-2 px-3 rounded text-center' 
+            style={{ 
+                backgroundColor:`${props.bg_color}`, 
+                color:`${props.font_color}` 
+            }}
+        >
             <p>{props?.name}</p>
         </div>
     )
