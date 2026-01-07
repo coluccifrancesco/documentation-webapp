4be583d9d4c1baa49bbb790e954355f0232b7003
coluccifrancesco <coluccif2002@gmail.com>
2026-01-07 19:52:35 +0100

    created categories banner, finished single topic page, adjusted styles, created technologies list

--- documentation-react/src/components/Footer.jsx
+++ documentation-react/src/components/Footer.jsx
@@ -1,22 +1,8 @@
-export default function CategoriesBanner (props) {
+export default function Footer () {
 
-    // Prop structure:
-    // <DifficultyBanner grade_name={argument.difficulty.grade_name} grade_numerical={argument.difficulty.grade_numerical} />
-
-    const gradeProperties = {
-        1: {emojiClasses: 'fa-solid fa-face-laugh-beam', bg_color: 'bg-color-200', font_color: 'text-color-800'},
-        2: {emojiClasses: 'fa-solid fa-face-grin-wink', bg_color: 'bg-color-300', font_color: 'text-color-700'},
-        3: {emojiClasses: 'fa-solid fa-face-meh', bg_color: 'bg-color-400', font_color: 'text-color-200'},
-        4: {emojiClasses: 'fa-solid fa-face-flushed', bg_color: 'bg-color-500', font_color: 'text-color-100'},
-        5: {emojiClasses: 'fa-solid fa-face-dizzy', bg_color: 'bg-color-600', font_color: 'text-color-50'}
-    };
-
-    const config = gradeProperties[props.grade_numerical];
-
-    return (
-        <div className={`difficulty-banner d-flex justify-content-center align-items-center gap-2 py-2 px-3 rounded ${config?.bg_color} ${config?.font_color}`}>
-            <p>{props?.grade_name}</p>
-            <i className={config?.emojiClasses}></i>
-        </div>
-    )
+    return <footer>
+    
+    <h1>Footer</h1>
+    
+    </footer>
 }
--- documentation-react/src/pages/SingleTopic.jsx
+++ documentation-react/src/pages/SingleTopic.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getSingleArgument } from '../services/api';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import ReactMarkdown from 'react-markdown';
 import DifficultyBanner from '../components/DifficultyBanner';
-import CategoriesBanner from '../components/Footer';
+import TechnologyBanner from '../components/TechnologyBanner';
 import GoBackBtn from '../components/GoBackBtn';
 import ScrollToTopBtn from '../components/ScrollToTopBtn';
 
@@ -48,13 +48,24 @@ export default function SingleTopic() {
             <div className='d-flex justify-content-between align-items-center my-3'>
                 <p className='w-50'>{data?.resume || ''}</p>
 
-                <div>
+                <div className='d-md-flex justify-content-center align-items-center gap-4'>
+                    {data.technologies? <ul className='list-unstyled mb-3 mb-md-0'>
+
+                        {data.technologies.map(tech => (
+                            <li key={tech.id}>
+                                <Link to={`/technologies/${tech.id}`}>
+                                    <TechnologyBanner name={tech.name} bg_color={tech.bg_color} font_color={tech.font_color} />
+                                </Link>
+                            </li>
+                        ))}
+
+                    </ul> : ''}
+                    
                     <DifficultyBanner grade_name={data.difficulty.grade_name} grade_numerical={data.difficulty.grade_numerical} />
-                    {data.categories? <CategoriesBanner /> : ''}
                 </div>
             </div>
 
-            <div className='react-markdown p-4'>
+            <div className='react-markdown p-4 mt-5 mt-md-5'>
                 {data.md_text ? <ReactMarkdown
                     components={{
                         code({ node, inline, className, children, ...props }) {
