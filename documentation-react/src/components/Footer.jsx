c1f25c7ebf5f4441a200dd3cbec99765f5ff3079
coluccifrancesco <coluccif2002@gmail.com>
2026-01-10 19:10:57 +0100

    implemented footer links with relative aesthetics

--- documentation-react/src/components/Footer.jsx
+++ documentation-react/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Footer () {
 
     return <footer className="mt-5 row">
@@ -9,15 +11,35 @@ export default function Footer () {
         </div>
 
         <div className="col-12 col-md-6">
-            <div className="p-3 border">
-                <h1>Footer</h1>
+            <div className="p-3 border row footer-links">
+                <Link to='https://www.linkedin.com/in/francesco-colucci-589414290/' className="col-4">
+                    <div className="text-center">
+                        <i class="fa-brands fa-github"></i>
+                    </div>        
+                </Link>
+                
+                <Link to='https://github.com/coluccifrancesco' className="col-4">
+                    <div className="text-center">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                </Link>
+                
+                <Link to='http://127.0.0.1:8000/' className="col-4">
+                    <div className="text-center">
+                        <i class="fa-brands fa-laravel"></i>
+                    </div>    
+                </Link>
             </div>
         </div>
 
         <div className="col-12">
             <div className="p-3 border text-center">
                 <p>Created by: <b>Francesco Colucci</b></p>
-                <p><i>Boolean Code Academy</i></p>
+                
+                <Link to='https://boolean.careers/' >
+                    <p><i>Boolean Coding Academy</i></p>
+                </Link>
+                
                 <i>2025 - 2026</i>
             </div>
         </div>
