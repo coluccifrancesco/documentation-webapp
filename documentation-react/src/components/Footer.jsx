8b1f23d643c487dad372eafa113adc5abf543297
coluccifrancesco <coluccif2002@gmail.com>
2026-01-10 21:50:19 +0100

    adjusted margin on logo

--- documentation-react/src/components/Footer.jsx
+++ documentation-react/src/components/Footer.jsx
@@ -1,48 +1,38 @@
 import { Link } from "react-router-dom"
 
-export default function Footer () {
+export default function Footer() {
 
-    return <footer className="mt-5 row">
-    
-        <div className="col-12 col-md-6">
-            <div className="p-3 border">
-                <h1>Footer</h1>
-            </div>
+    return <footer className="mt-5">
+
+        <div className="p-3 row footer-links">
+            <Link to='https://www.linkedin.com/in/francesco-colucci-589414290/' className="col-4">
+                <div className="text-center">
+                    <i class="fa-brands fa-github"></i>
+                </div>
+            </Link>
+
+            <Link to='https://github.com/coluccifrancesco' className="col-4">
+                <div className="text-center">
+                    <i class="fa-brands fa-linkedin"></i>
+                </div>
+            </Link>
+
+            <Link to='http://127.0.0.1:8000/' className="col-4">
+                <div className="text-center">
+                    <i class="fa-brands fa-laravel"></i>
+                </div>
+            </Link>
         </div>
 
-        <div className="col-12 col-md-6">
-            <div className="p-3 border row footer-links">
-                <Link to='https://www.linkedin.com/in/francesco-colucci-589414290/' className="col-4">
-                    <div className="text-center">
-                        <i class="fa-brands fa-github"></i>
-                    </div>        
-                </Link>
-                
-                <Link to='https://github.com/coluccifrancesco' className="col-4">
-                    <div className="text-center">
-                        <i class="fa-brands fa-linkedin"></i>
-                    </div>
-                </Link>
-                
-                <Link to='http://127.0.0.1:8000/' className="col-4">
-                    <div className="text-center">
-                        <i class="fa-brands fa-laravel"></i>
-                    </div>    
-                </Link>
-            </div>
+        <div className="p-3 text-center">
+            <p>Created by: <b>Francesco Colucci</b></p>
+
+            <Link to='https://boolean.careers/' >
+                <p><i>Boolean Coding Academy</i></p>
+            </Link>
+
+            <i>2025 - 2026</i>
         </div>
 
-        <div className="col-12">
-            <div className="p-3 border text-center">
-                <p>Created by: <b>Francesco Colucci</b></p>
-                
-                <Link to='https://boolean.careers/' >
-                    <p><i>Boolean Coding Academy</i></p>
-                </Link>
-                
-                <i>2025 - 2026</i>
-            </div>
-        </div>
-    
     </footer>
 }
