12adc97172db5bc0c32b1c7fddcfeacc760003d1
coluccifrancesco <coluccif2002@gmail.com>
2026-01-10 18:51:30 +0100

    removed useless comment

--- documentation-react/src/components/Footer.jsx
+++ documentation-react/src/components/Footer.jsx
@@ -1,28 +1,26 @@
 export default function Footer () {
 
     return <footer className="mt-5 row">
-
-    {/* DA AGGIORNARE IL LAYOUT IN MODALITA TABLET E MOBILE */}
     
-    <div className="col-12 col-md-6">
-        <div className="p-3 border">
-            <h1>Footer</h1>
+        <div className="col-12 col-md-6">
+            <div className="p-3 border">
+                <h1>Footer</h1>
+            </div>
         </div>
-    </div>
 
-    <div className="col-12 col-md-6">
-        <div className="p-3 border">
-            <h1>Footer</h1>
+        <div className="col-12 col-md-6">
+            <div className="p-3 border">
+                <h1>Footer</h1>
+            </div>
         </div>
-    </div>
 
-    <div className="col-12">
-        <div className="p-3 border text-center">
-            <p>Created by: <b>Francesco Colucci</b></p>
-            <p><i>Boolean Code Academy</i></p>
-            <i>2025 - 2026</i>
+        <div className="col-12">
+            <div className="p-3 border text-center">
+                <p>Created by: <b>Francesco Colucci</b></p>
+                <p><i>Boolean Code Academy</i></p>
+                <i>2025 - 2026</i>
+            </div>
         </div>
-    </div>
     
     </footer>
 }
